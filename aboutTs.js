7e2e4ffb93bc1ce816137befe6806ac8bc082e7b
soozin-L <blablah825@gmail.com>
2021-09-29 07:39:10 +0900

    ts 사용하는 이유에 대한 간단 예시 1, 오류표시 2. 타입에 맞는 자동완성 기능

---
+++ aboutTs.js
@@ -0,0 +1,8 @@
+
+  // math.ts
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const result = sum("10", 50);
+  console.log(result)
---
+++ aboutTs.ts
@@ -0,0 +1,9 @@
+  function add(a: number, b: number) {
+    return a + b;
+  }
+
+  let total = (30, 50);
+  total.toLocaleString
+
+
+  
